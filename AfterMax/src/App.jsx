8997c7ded6d87dcea1f46a761057cd7c7385d131
shevang5 <shevangpalgam@gmail.com>
2026-07-28 18:04:56 +0530

    fixing error in delpoying

--- AfterMax/src/App.jsx
+++ AfterMax/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import ServicesPage from './pages/ServicesPage'
 import AboutPage from './pages/AboutPage'
 import PortfolioPage from './pages/PortfolioPage'
